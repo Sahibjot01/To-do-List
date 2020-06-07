7bdf4921a5c84cbc0a5730b51b73d92582b47cfd
sahibjot01 <sahibjot70@gmail.com>
2020-06-07 15:36:47 +0530

    Todo list website

---
+++ app.js
@@ -0,0 +1,44 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+const app = express();
+const items =["Buy food","Cook food", "Eat food"];
+const workItems = [];
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine" , "ejs");
+
+app.get("/", function(req,res){
+  day = date.getDate();
+  res.render("list" , {listTitle:day, newListItems:items});
+});
+
+app.get("/work", function(req,res){
+  res.render("list",{listTitle:"Work List",newListItems:workItems});
+});
+
+app.post("/work",function(req,res){
+
+  const item = req.body.newItem;
+
+});
+
+app.post("/",function(req,res){
+  const item = req.body.newItem;
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+app.get("/about" , function(req,res){
+  res.render("about");
+});
+
+app.listen(3000,function(){
+  console.log("started listen to port 3000.");
+});
